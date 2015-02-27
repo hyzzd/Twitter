8cd561f124e013827f42a6aba9f28463c3397a6f
Neal Wu <nwu@box.com>
2015-02-26 21:44:49 -0800

    Finished auto layout for ProfileViewController

---
+++ Twitter/ProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProfileViewController.h
+//  Twitter
+//
+//  Created by Neal Wu on 2/26/15.
+//  Copyright (c) 2015 Neal Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProfileViewController : UIViewController
+
+@end
---
+++ Twitter/ProfileViewController.m
@@ -0,0 +1,21 @@
+//
+//  ProfileViewController.m
+//  Twitter
+//
+//  Created by Neal Wu on 2/26/15.
+//  Copyright (c) 2015 Neal Wu. All rights reserved.
+//
+
+#import "ProfileViewController.h"
+
+@interface ProfileViewController ()
+
+@end
+
+@implementation ProfileViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+@end
